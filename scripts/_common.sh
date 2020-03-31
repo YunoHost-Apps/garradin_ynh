d17daf939d3b39ee2ebda0bed6cd39b41c02d978
Robles Rodolphe <rodolphe.robles@sfr.fr>
2020-03-31 18:30:14 +0200

    try to debug

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,7 +2,7 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-YNH_PHP_VERSION="7.2"
+YNH_PHP_VERSION="7.3"
 extra_pkg_dependencies="php${YNH_PHP_VERSION}-bz2 php${YNH_PHP_VERSION}-imap php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-apcu php${YNH_PHP_VERSION}-redis php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-sqlite3"
 
 # Install another version of php.
