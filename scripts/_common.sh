5245009c35424578ea3b77d9bded58d429c00cb5
Rodolphe Robles <rodolphe.robles@sfr.fr>
2020-04-04 03:34:57 +0200

    add hooks to copy file config.dist.php in config.local.php with good options

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="php7.0-sqlite3"
+
 # ============= FUTURE YUNOHOST HELPER =============
 # Delete a file checksum from the app settings
 #
