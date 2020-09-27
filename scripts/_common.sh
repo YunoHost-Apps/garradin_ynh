5ef87897584bc3ed3dfa3050b279ab0f1dbed578
Robles Rodolphe <rodolphe.robles@sfr.fr>
2020-09-28 00:49:07 +0200

    correct bad syntax in _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 # dependencies used by the app
 if [ "$lsb_release --codename --short" = "buster"]; then
   pkg_dependencies="php7.3-sqlite3"
-else [ "$lsb_release --codename --short" = "stretch"]; then
+else [ "$lsb_release --codename --short" = "stretch"]
   pkg_dependencies="php7.0-sqlite3"
 fi
 
