2c8542eaadc1a3e9e15693a66c7030394375d596
Robles Rodolphe <rodolphe.robles@sfr.fr>
2021-04-17 23:44:09 +0200

    debug script install

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,11 @@
 # COMMON VARIABLES
 #=================================================
 # dependencies used by the app
+#pkg_dependencies="imagemagick"
+
 YNH_PHP_VERSION="7.4"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3 hp${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-imap php${YNH_PHP_VERSION}-intl  php${YNH_PHP_VERSION}-cli"
+extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-imap php${YNH_PHP_VERSION}-intl  php${YNH_PHP_VERSION}-cli"
 
 #=================================================
 # PERSONAL HELPERS
