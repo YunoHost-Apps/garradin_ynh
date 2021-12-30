92b105e43db05dc912e5a9877096abeb785577a7
Florent <florent.git@zeteo.me>
2021-12-30 14:12:37 +0100

    Split config into several files

---
+++ conf/config.local.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * NE PAS MODIFIER CE FICHIER. Il est généré automatiquement à chaque mise à jour de Yunohost
+ *
+ * Modifiez plutôt le fichier config.local.user.php
+ */
+require '__FINALPATH__/config.local.yunohost.php';
+require '__FINALPATH__/config.local.user.php';
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@
 
 YNH_PHP_VERSION="7.4"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-intl  php${YNH_PHP_VERSION}-cli"
+extra_php_dependencies="php${YNH_PHP_VERSION} php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-imagick php${YNH_PHP_VERSION}-intl  php${YNH_PHP_VERSION}-cli"
 
 #=================================================
 # PERSONAL HELPERS
